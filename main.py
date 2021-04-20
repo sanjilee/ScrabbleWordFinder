850e505cd7b3910df3e348dc1d144bd3ca06ddcd
Sanji Lee <sleekguy321@gmail.com>
2021-04-20 18:13:08 +0100

    Actual stuff

---
+++ main.py
@@ -0,0 +1,53 @@
+letters = list(input("Letters: "))
+points = {
+    'A': 1,
+    'E': 1,
+    'I': 1,
+    'O': 1,
+    'U': 1,
+    'L': 1,
+    'N': 1,
+    'S': 1,
+    'T': 1,
+    'R': 1,
+    'D': 2,
+    'G': 2,
+    'B': 3,
+    'C': 3,
+    'M': 3,
+    'P': 3,
+    'F': 4,
+    'H': 4,
+    'V': 4,
+    'W': 4,
+    'Y': 4,
+    'K': 5,
+    'J': 8,
+    'X': 8,
+    'Q': 10,
+    'Z': 10
+}
+words = []
+with open('words.txt', 'r') as f:
+    for word in f:
+        copyLetters = letters.copy()
+        word = word.strip()
+        equal = True
+        for letter in word:
+            if letter in copyLetters:
+                copyLetters.remove(letter)
+            else:
+                equal = False
+                break
+        if equal:
+            words.append(word)
+wordValues = {}
+value = 0
+for word in words:
+    value = 0
+    for letter in word:
+        value += points[letter]
+    wordValues[word] = value
+sortedWords = {k: v for k, v in sorted(wordValues.items(), key=lambda item: item[1])}
+for key in sortedWords.keys():
+    print(f'{key}: {sortedWords[key]}')
